50d6361dcfcdacc310ccc949881dd594ac1e54bd
Robert Egeland <robegeland@yahoo.com>
2022-03-17 20:57:08 -0500

    commit tree d9273914aa501fee063eed3d1d9e20bb6106de46

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
-// create your App component here
+import React, {useEffect, useState} from "react";
+ 
+function App() {
+    const [pic, setPic] = useState()
+    const [isLoaded, setIsLoaded] = useState(false)
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then((res) => res.json())
+            .then((data) => {
+                setPic(data.message)
+                setIsLoaded(true)
+            })
+    }, [])
+
+    return isLoaded ? <img src={pic} alt="A Random Dog"></img> : <p>Loading...</p>
+
+}
+
+export default App;
+
